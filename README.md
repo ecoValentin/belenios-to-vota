3c08e5d113c3330caaace5cd5e7e975ad7e93115
Michael Faschinger <michivo@gmail.com>
2020-11-12 21:58:29 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # belenios-to-vota
 Converts ballot data from Belenios JSON format to Vota CSV format
+
+Vota (see https://gitlab.com/mtausig/vota ) is the ballot counting tool used by the Green Party in Vienna. It takes a CSV file containing all
+ballots and a CSV file containing all candidates and calculates the results of an election.
+
+Belenios (see https://gitlab.inria.fr/belenios/belenios ) is a verifiable voting system, also used by the Green Party in Austria. Belenios
+produces its results in a JSON format.
+
+This tool converts the JSON file produced by Belenios into a CSV file that can be used by Vota. In order to run it, you need Node.js. Clone the 
+repository and run 
+
+```
+node . [results.json] [canidates.csv] [out.csv]
+```
+
+Where results.json is the result file produced by Belenios. candidates.csv is the candidates file in the format needed by Vota, and out.csv 
+will contain the CSV data that can be fed into Vota.
+
+You can find sample files for all of the files in this repository. You may also use a JSON-format for the candidates file.
+
+# Important
+**Please make sure that the order of candidates in the canidates.csv file is the same as the order of candidates on the ballots in Belenios! Belenios does not include the candidate names in the output!**
---
+++ run.js
@@ -0,0 +1,73 @@
+const fs = require('fs');
+const { argv } = require('process');
+
+console.log('Starting conversion...')
+
+if(process.argv.length !== 5) {
+    console.log('Usage: node . [beleniosResultsFile] [candidatesFile] [outputFile]');
+    return;
+}
+
+fs.readFile(process.argv[2], 'utf8', (resultError, results) => {
+    if(resultError) {
+        console.log('Could not open results file: ');
+        console.error(resultError);
+        return;
+    }
+
+    fs.readFile(process.argv[3], 'utf8', (candidatesError, candidates) => {
+        if(candidatesError) {
+            console.log('Could not open results file: ');
+            console.error(candidatesError);
+            return;
+        }
+
+        const csvResults = convertResults(results, candidates);
+
+        fs.writeFile(process.argv[4], csvResults, err => {
+            if(err) {
+                console.log('Error writing to output file: ');
+                console.error(err);
+                return;
+            }
+
+            console.log('Successfully converted results!')
+        });
+    });
+});
+
+function convertResults(results, candidates) {
+    const resultData = JSON.parse(results);
+    const candidatesData = parseCandidates(candidates);
+
+    let resultCsv = '';
+    candidatesData.forEach(candidate => {
+        resultCsv += ';' + candidate;
+    });
+    resultCsv += '\r\n';
+   
+    resultData.forEach((ballot, index) => {
+        resultCsv += 'Ballot' + index;
+        ballot.forEach(vote => {
+            resultCsv += ';' + (vote === 0 ? '' : vote);
+        });
+        resultCsv += '\r\n';
+    }); 
+
+    return resultCsv;
+}
+
+function parseCandidates(candidates) {
+    try {
+        return JSON.parse(candidates);
+    } catch (err) {
+        let candidatesData = [];
+        candidatesLines = candidates.split('\r\n');
+        candidatesLines.forEach(line => {
+            const semicolonIdx = line.indexOf(';');
+            candidatesData.push(line.substr(0, semicolonIdx));
+        });
+
+        return candidatesData;
+    }
+}
